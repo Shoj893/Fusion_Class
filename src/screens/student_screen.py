dee352f114130de21348e4955b13eb79f65e6116
Shobhit Jaiswal <shobhitjais2607@gmail.com>
2026-05-08 22:50:18 +0530

    Fixed the Subject enrollment error on student dashboard

--- src/screens/student_screen.py
+++ src/screens/student_screen.py
@@ -107,7 +107,8 @@ def student_dashboard():
 
         stats = stats_map.get(sid,{"total":0, "attended": 0} )
         def unenroll_button():
-                if st.button("Unenroll from this course", type='tertiary', width='stretch', icon=':material/delete_forever:'):
+                # Use a unique key for each subject card button to avoid Streamlit duplicate element IDs.
+                if st.button("Unenroll from this course", type='tertiary', width='stretch', icon=':material/delete_forever:', key=f"unenroll_{sid}"):
                     unenroll_student_to_subject(student_id, sid)
                     st.toast(f'Unenrolled from {sub['name']} successfully!')
                     st.rerun()
